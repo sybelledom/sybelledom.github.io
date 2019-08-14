69e22060409a1b6505121fb9bcfa02bc94be2274
Manueldevries <manuel@webdesignord.fr>
2019-08-14 18:19:58 +0200

    add header todo footer + content

---
+++ _pages/1_accueil.md
@@ -0,0 +1,5 @@
+---
+layout: accueil
+title: Accueil
+permalink: /accueil/
+---
--- _site/js/hello.js
+++ _site/js/hello.js
@@ -1,1 +1,7 @@
 console.log('hello you ❤️');
+
+document
+  .querySelectorAll('.header__nav a')
+  .map(i => i.classList.toggle('actif'));
+
+document.querySelectorAll('.header__nav a')[0].classList.add('actif');
