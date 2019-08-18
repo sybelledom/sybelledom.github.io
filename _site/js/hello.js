cee4510cea047100b4da9d286623ae2990351b02
Becquet <bq_sketch_sharelib>
2019-08-18 19:08:42 +0200

    Add mdr ingredients.page TOFIX windows.location refresh loss id

--- _site/js/hello.js
+++ _site/js/hello.js
@@ -5,3 +5,14 @@ const navList = Array.from(document.querySelectorAll('.header__nav a'))
 navList[0].classList.toggle('actif')
 
 console.log(navList)
+
+function go_id(params) {
+  // window.location === 'http://localhost:4000/ingredients/'
+  //   ? console.log('ok')
+  //   : (window.location = 'http://localhost:4000/ingredients/')
+  var id = document.getElementById('ref').value
+  console.log(id)
+  document.getElementById(`_${id}`).classList.add('activline')
+  window.location =
+    window.location + 'index.html#_' + document.getElementById('ref').value
+}
