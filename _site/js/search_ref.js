ae68c3a97a163718841511b1069ceef50023471b
Manueldevries <manuel@webdesignord.fr>
2019-08-20 14:20:51 +0200

    FIX Safari BUG by add e.preventDefault

---
+++ _site/js/search_ref.js
@@ -0,0 +1,57 @@
+// traitement saisie interdiction lettre
+const refInput = document.getElementById('ref');
+let ref_ = '';
+const allRef = [...document.querySelectorAll('tr td:first-child')];
+const allRefNorm = allRef.map(i => normalizSpace(i.innerHTML));
+
+// add keypress listener
+document.addEventListener('keypress', refInputKey);
+console.log(allRefNorm);
+
+function isPresent(arg, ref) {
+  // console.log(`isPresent dit ref: ${ref}, ça vaut ${arg.includes(ref)}?`)
+  if (arg.includes(ref)) {
+    console.log(`ref: ${ref} existe`);
+    go_id(ref_);
+  } else {
+    alert("votre référence n'est pas valide.");
+    window.location =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      '/ingredients/index.html';
+  }
+}
+
+function refInputKey(e) {
+  e.preventDefault(); // important FIX bug for safari browser
+  console.log(e.key);
+  ref_ += e.key;
+  console.log(ref_);
+  if (isFinite(e.key) || e.key == 'Enter') {
+    if (ref_.length === 7) {
+      isPresent(allRefNorm, ref_);
+      ref_ = '';
+    }
+  } else {
+    alert('Oups juste des chiffres :)');
+  }
+}
+
+function normalizSpace(string) {
+  return string.replace(/[\n\r\s\t]+/g, '');
+}
+
+function go_id(params) {
+  if (params) {
+    window.location =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      '/ingredients/index.html' +
+      '#ref' +
+      params;
+
+    document.getElementById(`ref${params}`).classList.add('activline');
+  }
+}
