762d9740cee1b2e2faa44a4b7b26ab9178b33511
Manueldevries <manuel@webdesignord.fr>
2019-08-22 10:18:32 +0200

    UPDATE _data/ingredients.json

--- _site/js/search_ref.js
+++ _site/js/search_ref.js
@@ -1,84 +1,122 @@
-// traitement saisie interdiction lettre
-const refInput = document.getElementById('ref')
-let ref_ = ''
-const allRef = [...document.querySelectorAll('tr td:first-child')]
-const allRefNorm = allRef.map(i => normalizSpace(i.innerHTML))
+"use strict";
 
-// listener
-document.getElementById('ref').addEventListener('keypress', refInputKey)
-document.getElementById('search_btn').addEventListener('click', btn_action)
-// console.log(allRefNorm);
+function _toConsumableArray(arr) {
+  return (
+    _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread()
+  );
+}
 
-function isPresent (arg, ref) {
-  console.log(
-    `isPresent dit ref: ${ref} === ${ref_}, ça vaut ${arg.includes(ref)}?`
+function _nonIterableSpread() {
+  throw new TypeError("Invalid attempt to spread non-iterable instance");
+}
+
+function _iterableToArray(iter) {
+  if (
+    Symbol.iterator in Object(iter) ||
+    Object.prototype.toString.call(iter) === "[object Arguments]"
   )
+    return Array.from(iter);
+}
+
+function _arrayWithoutHoles(arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) {
+      arr2[i] = arr[i];
+    }
+    return arr2;
+  }
+}
+
+// traitement saisie interdiction lettre
+var refInput = document.getElementById("ref");
+var ref_ = "";
+
+var allRef = _toConsumableArray(document.querySelectorAll("tr td:first-child"));
+
+var allRefNorm = allRef.map(function(i) {
+  return normalizSpace(i.innerHTML);
+}); // listener
+
+document.getElementById("ref").addEventListener("keypress", refInputKey);
+document.getElementById("search_btn").addEventListener("click", btn_action); // console.log(allRefNorm);
+
+function isPresent(arg, ref) {
+  console.log(
+    "isPresent dit ref: "
+      .concat(ref, " === ")
+      .concat(ref_, ", \xE7a vaut ")
+      .concat(arg.includes(ref), "?")
+  );
+
   if (arg.includes(ref)) {
-    console.log(`ref: ${ref} existe`)
-    go_id(ref_)
+    console.log("ref: ".concat(ref, " existe"));
+    go_id(ref_);
   } else {
-    alert("votre référence n'est pas valide.")
+    alert("votre référence n'est pas valide.");
     window.location =
       window.location.protocol +
-      '//' +
+      "//" +
       window.location.host +
-      '/ingredients/index.html'
+      "/ingredients/index.html";
   }
 }
 
-function refInputKey (e) {
-  console.log(e.key)
-  ref_ += e.key
-  console.log(ref_)
-  if (isFinite(e.key) || e.key == 'Enter') {
+function refInputKey(e) {
+  console.log(e.key);
+  ref_ += e.key;
+  console.log(ref_);
+
+  if (isFinite(e.key) || e.key == "Enter") {
     if (ref_.length === 7) {
-      e.preventDefault() // important FIX bug for safari browser
-      isPresent(allRefNorm, ref_)
-      ref_ = ''
+      e.preventDefault(); // important FIX bug for safari browser
+
+      isPresent(allRefNorm, ref_);
+      ref_ = "";
     }
   } else {
-    alert('Oups juste des chiffres :)')
+    alert("Oups juste des chiffres :)");
   }
 }
 
-function normalizSpace (string) {
-  return string.replace(/[\n\r\s\t]+/g, '')
+function normalizSpace(string) {
+  return string.replace(/[\n\r\s\t]+/g, "");
 }
 
-function go_id (params) {
+function go_id(params) {
   if (params) {
     window.location =
       window.location.protocol +
-      '//' +
+      "//" +
       window.location.host +
-      '/ingredients/index.html' +
-      '#ref' +
-      params
-
-    document.getElementById(`ref${params}`).classList.add('activline')
+      "/ingredients/index.html" +
+      "#ref" +
+      params;
+    document.getElementById("ref".concat(params)).classList.add("activline");
   }
 }
 
-function btn_action (e) {
-  e.preventDefault()
-  go_id(refInput.value)
-}
+function btn_action(e) {
+  e.preventDefault();
+  go_id(refInput.value);
+} // gestion supp : backspace + supp
 
-// gestion supp : backspace + supp
-document.getElementById('ref').addEventListener('keydown', KeyCheck)
+document.getElementById("ref").addEventListener("keydown", KeyCheck);
 
-function KeyCheck (event) {
-  var KeyID = event.keyCode
+function KeyCheck(event) {
+  var KeyID = event.keyCode;
+
   switch (KeyID) {
     case 8:
       // gérer le cas d'une suppression suite à une selection
-      ref_ = ref_.slice(0, -1)
-      console.log(`bckspa ref_: ${ref_}`)
-      break
+      ref_ = ref_.slice(0, -1);
+      console.log("bckspa ref_: ".concat(ref_));
+      break;
+
     case 46:
-      console.log('delete')
-      break
+      console.log("delete");
+      break;
+
     default:
-      break
+      break;
   }
 }
